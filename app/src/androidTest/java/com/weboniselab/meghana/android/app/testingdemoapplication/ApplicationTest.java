e75e7abd518e158aef927fb1b36bba8688627f83
meghana-webonise <meghana.shastri@weboniselab.com>
2015-09-30 18:14:08 +0530

    First commit - working test cases

---
+++ app/src/androidTest/java/com/weboniselab/meghana/android/app/testingdemoapplication/ApplicationTest.java
@@ -0,0 +1,71 @@
+package com.weboniselab.meghana.android.app.testingdemoapplication;
+
+import android.app.Application;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ApplicationTestCase;
+import android.test.UiThreadTest;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mainActivity;
+    private TextView tv;
+
+    public ApplicationTest(){
+        super(MainActivity.class);
+    }
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity= getActivity();
+        assertNotNull(mainActivity);
+        tv= (TextView)mainActivity.findViewById(R.id.tv);
+
+    }
+
+    @MediumTest
+    public void testOpenNextActivity() {
+        // register next activity that need to be monitored.
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(NewActivity.class.getName(), null, false);
+        final Button button = (Button) mainActivity.findViewById(R.id.btn);
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // click button and open next activity.
+                button.performClick();
+            }
+        });
+
+        NewActivity newActivity = (NewActivity) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5);
+        // next activity is opened and captured.
+        assertNotNull(newActivity);
+        newActivity .finish();
+    }
+
+    @SmallTest
+    public void testTextView() {
+        assertNotNull("text view is null", tv);
+    }
+
+    @SmallTest
+    public void testTextViewContent() {
+        assertEquals("Text view text is incorrect", tv.getText().toString(), mainActivity.getString(R.string.hello_world));
+    }
+
+    @SmallTest
+    public void testMultiply() {
+        assertEquals("10 x 5 must be 50", 50, 10 * 5);
+    }
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
---
+++ app/src/main/java/com/weboniselab/meghana/android/app/testingdemoapplication/NewActivity.java
@@ -0,0 +1,15 @@
+package com.weboniselab.meghana.android.app.testingdemoapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by webonise on 30/9/15.
+ */
+public class NewActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+    }
+}
